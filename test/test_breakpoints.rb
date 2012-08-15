760f94236d5c592bd872386cd4caf82dea081d96
Dennis Ushakov <dennis.ushakov@gmail.com>
2012-08-15 15:05:03 +0400

    breakpoints test added

---
+++ test/test_breakpoints.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require File.expand_path("helper", File.dirname(__FILE__))
+
+# Some tests of Debugger module in C extension ruby_debug
+class TestBreakpoints < Test::Unit::TestCase
+  def test_find
+    Debugger.start_
+    Debugger.add_breakpoint("foo.rb", 11)
+    assert_not_nil(Debugger::Breakpoint.find(Debugger.breakpoints, "foo.rb", 11))
+    assert_nil(Debugger::Breakpoint.find(Debugger.breakpoints, "bar.rb", 11))
+    assert_nil(Debugger::Breakpoint.find(Debugger.breakpoints, "foo.rb", 10))
+    Debugger.stop
+  end
+end
