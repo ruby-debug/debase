e6a58f20fa5c5301f0ae965cab1a2af377ed2117
Oleg Sukhodolsky <os97673@gmail.com>
2015-07-05 20:34:24 +0300

    Need to use clacled_stack_size for stepping

--- ext/context.c
+++ ext/context.c
@@ -335,7 +335,7 @@ Context_stop_frame(VALUE self, VALUE frame)
   Data_Get_Struct(self, debug_context_t, debug_context);
   if(FIX2INT(frame) < 0 && FIX2INT(frame) >= debug_context->stack_size)
     rb_raise(rb_eRuntimeError, "Stop frame is out of range.");
-  debug_context->stop_frame = debug_context->stack_size - FIX2INT(frame);
+  debug_context->stop_frame = debug_context->calced_stack_size - FIX2INT(frame);
 
   return frame;
 }
--- ext/debase_internals.c
+++ ext/debase_internals.c
@@ -285,7 +285,7 @@ process_return_event(VALUE trace_point, void *data)
 
   --context->calced_stack_size;
   update_stack_size(context);
-  if(context->stack_size == context->stop_frame)
+  if(context->calced_stack_size == context->stop_frame)
   {
       context->stop_next = 1;
       context->stop_frame = 0;
