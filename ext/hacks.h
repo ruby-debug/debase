ae32e6eabc30fd3169cf92b4f68d65e8b63e3d88
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-10-13 21:01:13 +0300

    let's try using real stack size for stepping

--- ext/hacks.h
+++ ext/hacks.h
@@ -10,8 +10,8 @@ update_stack_size(debug_context_t *context)
   thread = ruby_current_thread;
   /* see backtrace_each in vm_backtrace.c */
   context->stack_size = (int)(RUBY_VM_END_CONTROL_FRAME(thread) - thread->cfp - 1);
-  if (CTX_FL_TEST(context, CTX_FL_UPDATE_STACK)) {
-    context->calced_stack_size = context->stack_size;
-    CTX_FL_UNSET(context, CTX_FL_UPDATE_STACK);
-  }
+  // if (CTX_FL_TEST(context, CTX_FL_UPDATE_STACK)) {
+  //   context->calced_stack_size = context->stack_size;
+  //   CTX_FL_UNSET(context, CTX_FL_UPDATE_STACK);
+  // }
 }
