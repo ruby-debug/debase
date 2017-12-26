c53f1f0a2ee4d76d009a014c7539d84339cb54e8
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2017-12-26 15:11:59 +0300

    cleanup

--- test/test_catchpoint.rb
+++ test/test_catchpoint.rb
@@ -6,7 +6,7 @@ require File.expand_path("helper", File.dirname(__FILE__))
 class TestRubyDebugCatchpoint < Test::Unit::TestCase
   def test_catchpoints
     Debugger.init_variables
-    assert_raise(RuntimeError) {puts Debugger.catchpoints}
+    assert_raise(RuntimeError) {Debugger.catchpoints}
     Debugger.start_
     assert_equal({}, Debugger.catchpoints)
     Debugger.add_catchpoint('ZeroDivisionError')
