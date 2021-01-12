208c785a8468d74ee6772db352918a27efd10453
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2021-01-12 11:38:57 +0300

    After moving catchpoints to native, invoking catchpoints don't throw an error

--- test/test_catchpoint.rb
+++ test/test_catchpoint.rb
@@ -5,7 +5,7 @@ require File.expand_path("helper", File.dirname(__FILE__))
   
 class TestRubyDebugCatchpoint < Test::Unit::TestCase
   def test_catchpoints
-    assert_raise(RuntimeError) {Debugger.catchpoints}
+    assert_equal({}, Debugger.catchpoints)
     Debugger.start_
     assert_equal({}, Debugger.catchpoints)
     Debugger.add_catchpoint('ZeroDivisionError')
